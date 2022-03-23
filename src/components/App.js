9353e2ad32a0c24027a3fabc642d0e568c1582fd
e21001 <e21001@std.it-college.ac.jp>
2022-03-23 21:57:16 +0900

    TODO一覧を状態別に表示させる

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,18 @@ function App() {
     fetchData()
   }, [])
 
+  const inCompletedList = todoList.filter((todo) => {
+    return !todo.done
+  })
+
+  console.log(inCompletedList)
+
+  const completedList = todoList.filter((todo) => {
+    return todo.done
+  })
+
+  console.log(completedList)
+
   return (
     <>
       <h1>TODO進捗管理</h1>
@@ -23,11 +35,30 @@ function App() {
 
       <button>+ TODOを追加</button>
 
-      <h2>TODOリスト</h2>
+      <h2>未完了TODOリスト</h2>
       <ul>
-        {todoList.map((todo) => (
+        {inCompletedList.map((todo) => (
           <li key={todo.id}>
-            {todo.content}({todo.done ? "完了" : "未完了"})
+            {todo.content}
+            <button>
+              {todo.done ? "未完了リストへ" : "完了リストへ"}
+            </button>
+
+            <button>削除</button>
+          </li>
+        ))}
+      </ul>
+
+      <h2>完了TODOリスト</h2>
+      <ul>
+        {completedList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}
+            <button>
+              {todo.done ? "未完了リストへ" : "完了リストへ"}
+            </button>
+
+            <button>削除</button>
           </li>
         ))}
       </ul>
